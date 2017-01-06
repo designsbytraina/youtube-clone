35b31893b1ebbf9c7f9ce7ae072637ffe11ace73
Rachel Traina-Grandon <racheltrainagrandon@gmail.com>
2017-01-06 15:34:39 -0800

    Added initial input handling for Search Bar.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -5,8 +5,32 @@ import React, { Component } from 'react';
 // }
 
 class SearchBar extends Component {
+	constructor (props) {
+		super(props);
+
+		this.state = { 
+			term: '' 
+		};
+	}
+
 	render () {
-		return <input />;
+		return (
+			<div>
+				<input onChange={(e) => this.setState({term: e.target.value})} />
+				Value of the input: {this.state.term}
+			</div>
+		);
+		
+		// return <input onChange={(e) => console.log(e.target.value)} />;
+		// <input onChange={this._handleInputChange} />
+	}
+
+	_handleInputChange (e) {
+		// e.preventDefault();
+		// console.log(e.target.value);
+		this.setState({
+			term: e.target.value
+		});
 	}
 }
 
